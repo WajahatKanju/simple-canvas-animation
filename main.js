b98a67dd4c42ed9a6bf19da57439384c5f136274
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-08-28 10:17:57 -0700

    added simple animation

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-import { grid } from './drawing.js' ;
+import { grid } from './drawing.js';
 
 const canvas = document.getElementsByTagName('canvas')[0];
+const context = canvas.getContext('2d');
 
+context.strokeStyle = "white";
+context.lineWidth = 1.5;
 
-grid(canvas, context);
+let x =  40, y = context.canvas.height / 2;
+let dx = 1;
+function frame() {
+  context.clearRect(0, 0, context.canvas.width, context.
+    canvas.height);
+  draw(context);
+  update();
+}
+
+function update() {
+  x += dx;
+  
+}
+function draw(context) {
+  grid(canvas, context);
+  context.beginPath();
+  context.arc(x, y, 40, 0, 2 * Math.PI);
+  context.fill();
+  context.stroke();
+}
+
+setInterval(frame, 1000/60);
